a94ac39d0f2c3c04b912a8b38d16c396a7e04a4e
Kufreabasi <engineer_kufre@yahoo.com>
2022-07-09 11:50:18 +0100

    Use context in Favorites component

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>
+  const context = useContext(FavoritesContext);
+
+  let content =
+    context.totalFavorites === 0 ? (
+      <p>You do not have any favorites</p>
+    ) : (
+      <MeetupList meetups={context.favorites} />
+    );
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
-export default FavoritesPage
+export default FavoritesPage;
