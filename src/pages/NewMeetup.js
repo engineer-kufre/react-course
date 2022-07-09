11a0ddd9cd4eba3d0e5c78eb1bb4a0af4854be1c
Kufreabasi <engineer_kufre@yahoo.com>
2022-07-09 10:07:39 +0100

    Render Firebase db data

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,31 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm"
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  return <section>
-    <h1>Add New Meetup</h1>
-    <NewMeetupForm />
-  </section>
+  const navigate = useNavigate();
+
+  function handleAddMeetup(meetupData) {
+    fetch(
+      "https://meetup-app-336e4-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      navigate('/', {replace: true});
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
+    </section>
+  );
 }
 
-export default NewMeetupPage
+export default NewMeetupPage;
